734e5096f3049348e9a0349f276d58287370e400
PKwoon23 <matungcute@gmail.com>
2025-03-20 22:16:06 +0700

    Update next.config.mjs for export configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cdn.pixabay.com',
-            },
-        ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.pixabay.com',
+        },
+      ],
     },
-};
-
-export default nextConfig;
+    // Add the export path if needed
+    distDir: 'out', // The default folder for the export is "out"
+  };
+  
+  export default nextConfig;
+  
