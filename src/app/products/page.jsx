d68a8956bf277383844ac72e7b1a21de2d24c978
PKwoon23 <matungcute@gmail.com>
2025-03-21 10:21:35 +0700

    Edit pictures

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -15,7 +15,7 @@ export default function ProductsPage() {
         {products.map((product) => (
           <li key={product.id} className={styles.productItem}>
             <h2 className={styles.productName}>{product.name}</h2>
-            <Image
+            <Img
             src={product.images}
             alt='รูปสินค้า'
             width={300}
