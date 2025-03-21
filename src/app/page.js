d95d1b91b9f4a2c1e104187fadb352f6d0005d21
PKwoon23 <matungcute@gmail.com>
2025-03-21 12:39:21 +0700

    Edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Nav from "./components/Nav";
-import NextLogo from "/public/next.svg";
 
 export default function HomePage() {
   return (
@@ -9,20 +8,20 @@ export default function HomePage() {
       <hr className="my-3 border-gray-300" /> <br />
       <h3 className="text-4xl font-semibold text-center text-gray-800">Cat Shop</h3>
       <p className="text-lg text-center text-gray-600 mt-3">
-      เติมความสุขให้แมวของคุณ กับอาหารที่ดีที่สุดจากเรา! 🍽️🐾
+        เติมความสุขให้แมวของคุณ กับอาหารที่ดีที่สุดจากเรา! 🍽️🐾
       </p>
 
       <div className="flex justify-center mt-6">
         <Image
           src="https://cdn.pixabay.com/photo/2016/05/23/16/51/cat-1410745_1280.jpg"
-          width={300}
-          height={300}
-          priority 
+          width={1200}  // ขนาดภาพต้นฉบับ
+          height={800}  // สัดส่วนของภาพ
+          priority
           alt="cat image"
           className="rounded-lg shadow-lg"
+          style={{ objectFit: "cover" }} // ทำให้ภาพรักษาสัดส่วนและไม่บิดเบือน
         />
       </div>
     </main>
   );
 }
-
